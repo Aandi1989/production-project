75fc66d21017772eb2517da01091c3eb8faf2b01
Aandi1989 <navaliny1989@gmail.com>
2023-09-23 22:09:41 +0300

    14.1. Sidebar , layout of application

---
+++ src/widgets/SideBar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,19 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { useState } from "react";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className,])}
+    >
+        <button>toggle</button>
+    </div>
+  );
+};
