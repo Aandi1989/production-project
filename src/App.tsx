3fafdd8083e803d3b46450f3941b2de208d634b5
Aandi1989 <navaliny1989@gmail.com>
2023-09-23 16:35:26 +0300

    7.2. organize styles, add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import {render} from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
-   <BrowserRouter>
+  <BrowserRouter>
+    <ThemeProvider>
       <App />
-   </BrowserRouter>,
-   document.getElementById('root')
-)
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
