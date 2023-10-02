709a4d4a7ea6fef9e790ea75e044bfd8ffa5f3be
Aandi1989 <navaliny1989@gmail.com>
2023-10-02 21:10:36 +0300

    25.1 Setting Storybook

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,72 @@
+// import type { Meta, StoryObj } from '@storybook/react';
+
+// import { Button } from './Button';
+
+// const meta: Meta<typeof Button> = {
+//   title: 'ui/Button',
+//   component: Button,
+//   parameters: {
+//   },
+//   tags: ['autodocs'],
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// };
+
+// export default meta;
+// type Story = StoryObj<typeof Button>;
+
+// export const Primary: Story = {
+//   args: {
+//     primary: true,
+//     label: 'Button',
+//   },
+// };
+
+// export const Secondary: Story = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+
+
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Button, ThemeButton } from './Button';
+
+export default {
+    title: 'shared/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Text',
+};
+
+export const Clear = Template.bind({});
+Clear.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR,
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
+};
+
+export const OutlineDark = Template.bind({});
+OutlineDark.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
+};
+OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
